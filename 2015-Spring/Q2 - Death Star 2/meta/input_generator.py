7e4d2aefa846f00a5ae7d653850c6bf5fb598aaa
Andrew Sosa <sosa@cs.fsu.edu>
2015-03-02 14:07:53 -0500

    Created Input and Output for Q2.

--- 2015-Spring/Q2 - Death Star 2/meta/input_generator.py
+++ 2015-Spring/Q2 - Death Star 2/meta/input_generator.py
@@ -1,6 +1,8 @@
 # Generates random input
 import string
 import random
+from random import shuffle
+
 
 #w = int(raw_input("How many sets to create: "))
 w = int(raw_input())
@@ -24,8 +26,25 @@ for ii in range(0, w):
         temp = []
         temp.append(letters[i])
         temp.append(letters[i+1])
-        pairings.append(temp)
+        pairings.append(str(temp))
 
+    #print pairings
+
+    # Save unshuffled copy
     y = str(pairings)
+
+    # Shuffle the list
+    shuffle(pairings)
+
+    # This is our shuffled copy
+    z = str(pairings)
+
+    # Clean up both representations of the lists
     y = y.replace('\'', '')
+    y = y.replace('\"', '')
+    z = z.replace('\'', '')
+    z = z.replace('\"', '')
+
+    # Print both
     print y
+    print z
