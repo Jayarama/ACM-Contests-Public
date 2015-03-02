a03aa53e25c336a6f971ce7a2b74281774e733b3
Andrew Sosa <sosa@cs.fsu.edu>
2015-03-02 14:37:26 -0500

    Did a thing. pls ignore.

---
+++ 2015-Spring/Q2 - Garbage Compactor/solutions/Java/GarbageCompactor.java
@@ -0,0 +1,22 @@
+
+
+public class GarbageCompactor {
+
+    public static void main(String args[]) {
+
+
+
+
+    }
+
+
+    public static class Pair {
+
+        char x;
+        char y;
+
+        Pair(char x, char y)
+
+    }
+
+}
